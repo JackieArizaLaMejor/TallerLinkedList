958d054134655f204151758f48d5a34e13eaa3fd
User1 <User@email.es>
2024-09-21 10:06:51 -0500

    implemando varibles y sus metodos

--- src/Gestion_Biblioteca/Libro.java
+++ src/Gestion_Biblioteca/Libro.java
@@ -3,12 +3,63 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package Gestion_Biblioteca;
-
 /**
  *
  * @author USUARIO
  */
 public class Libro {
+    
+    public String titulo;
+    public String autor;
+    public String isbn;
+    public boolean estadoPrestamo;
+
+    public Libro(String titulo, String autor, String isbn, boolean estadoPrestamo) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.estadoPrestamo = estadoPrestamo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isEstadoPrestamo() {
+        return estadoPrestamo;
+    }
+
+    public void setEstadoPrestamo(boolean estadoPrestamo) {
+        this.estadoPrestamo = estadoPrestamo;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" + "titulo=" + titulo + ", autor=" + autor + ", isbn=" + isbn + ", estadoPrestamo=" + estadoPrestamo + '}';
+    }
+    
+    
+    
 
     
 }
--- src/Gestion_Biblioteca/Prestamo.java
+++ src/Gestion_Biblioteca/Prestamo.java
@@ -10,4 +10,59 @@ package Gestion_Biblioteca;
  */
 public class Prestamo {
     
+    String libroPrestado;
+    int usuarioPrestamo;
+    String fechaPrestamo;
+    String fechaDevolucion;
+
+    public Prestamo(String libroPrestado, int usuarioPrestamo, String fechaPrestamo, String fechaDevolucion) {
+        this.libroPrestado = libroPrestado;
+        this.usuarioPrestamo = usuarioPrestamo;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public String getLibroPrestado() {
+        return libroPrestado;
+    }
+
+    public void setLibroPrestado(String libroPrestado) {
+        this.libroPrestado = libroPrestado;
+    }
+
+    public int getUsuarioPrestamo() {
+        return usuarioPrestamo;
+    }
+
+    public void setUsuarioPrestamo(int usuarioPrestamo) {
+        this.usuarioPrestamo = usuarioPrestamo;
+    }
+
+    public String getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(String fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public String getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(String fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    @Override
+    public String toString() {
+        return "Prestamo{" + "libroPrestado=" + libroPrestado + ", usuarioPrestamo=" + usuarioPrestamo + ", fechaPrestamo=" + fechaPrestamo + ", fechaDevolucion=" + fechaDevolucion + '}';
+    }
+    
+    
+    
+
+   
+    
+    
 }
