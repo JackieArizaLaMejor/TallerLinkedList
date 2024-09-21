926969b4dec19751ff200545cfffcafded7d8dbc
jj <anonim@gmail.com>
2024-09-21 10:04:34 -0500

    Here is class usuario

--- src/Gestion_Biblioteca/Usuario.java
+++ src/Gestion_Biblioteca/Usuario.java
@@ -4,10 +4,56 @@
  */
 package Gestion_Biblioteca;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author USUARIO
  */
 public class Usuario {
+    public String nombre;
+    public int ID;
+    LinkedList<Prestamo> prestamosActuales = new LinkedList<>();
     
+
+    public Usuario(String nombre, int ID) {
+        this.nombre = nombre;
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public LinkedList<Prestamo> getPrestamosActuales() {
+        return prestamosActuales;
+    }
+
+    public void setPrestamosActuales(LinkedList<Prestamo> prestamosActuales) {
+        this.prestamosActuales = prestamosActuales;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", ID=" + ID + ", prestamosActuales=" + prestamosActuales + '}';
+    }
+    
+    public void agregarPrestamo(Prestamo p){
+    prestamosActuales.add(p);
+    }
+    public void eliminarPrestamo(Prestamo p){
+    prestamosActuales.remove(p);
+    }
 }
