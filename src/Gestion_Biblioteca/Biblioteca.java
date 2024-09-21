93cad0396b2d66a269c91c1641bd934b37d3a515
User1 <User@email.es>
2024-09-21 10:20:00 -0500

    Arreglando clase biblioteca

--- src/Gestion_Biblioteca/Biblioteca.java
+++ src/Gestion_Biblioteca/Biblioteca.java
@@ -1,53 +1,50 @@
-
 package Gestion_Biblioteca;
 
 import java.util.LinkedList;
 
 public class Biblioteca {
 
-    public static void main(String[] args) {
-        
-<<<<<<< Updated upstream
-        LinkedList<Libro> libro = new LinkedList<>();
-        LinkedList<Usuario> usuario = new LinkedList<>();
-        LinkedList<Prestamo> prestamo = new LinkedList<>();
-        
-        
+    LinkedList<Libro> libro = new LinkedList<>();
+    LinkedList<Usuario> usuario = new LinkedList<>();
+    LinkedList<Prestamo> prestamo = new LinkedList<>();
 
-=======
+    public void agregarLibro() {
+
     }
-    public void agregarLibro(){
-        
-        }
-    public void registrarLibro(){
-    
+
+    public void registrarLibro() {
+
     }
-    public void prestarLibro(){
-    
->>>>>>> Stashed changes
+
+    public void prestarLibro() {
+
     }
-    
-    public void devolverLibro(){
-    
+
+    public void devolverLibro() {
+
     }
-    
-    public void mostrarTodosLosLibros(){
-    
+
+    public void mostrarTodosLosLibros() {
+
     }
-    public void mostrarTodosLosUsuarios(){
-    
+
+    public void mostrarTodosLosUsuarios() {
+
     }
-    public void mostrarPrestamosActivos(){
-    
+
+    public void mostrarPrestamosActivos() {
+
     }
-    public void buscarLibroPorIsbn(){
-    
+
+    public void buscarLibroPorIsbn() {
+
     }
-    public void buscarUsuarioPorId(){
-    
+
+    public void buscarUsuarioPorId() {
+
     }
-    
-    public void listaPrestamosVencidos(){
-    
+
+    public void listaPrestamosVencidos() {
+
     }
 }
