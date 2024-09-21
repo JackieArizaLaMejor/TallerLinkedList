7d9183dda2c763e27d435d6b52d39cf06a7f0830
jj <anonim@gmail.com>
2024-09-21 10:18:23 -0500

    class library

--- src/Gestion_Biblioteca/Biblioteca.java
+++ src/Gestion_Biblioteca/Biblioteca.java
@@ -7,12 +7,47 @@ public class Biblioteca {
 
     public static void main(String[] args) {
         
+<<<<<<< Updated upstream
         LinkedList<Libro> libro = new LinkedList<>();
         LinkedList<Usuario> usuario = new LinkedList<>();
         LinkedList<Prestamo> prestamo = new LinkedList<>();
         
         
 
+=======
+    }
+    public void agregarLibro(){
+        
+        }
+    public void registrarLibro(){
+    
+    }
+    public void prestarLibro(){
+    
+>>>>>>> Stashed changes
     }
     
+    public void devolverLibro(){
+    
+    }
+    
+    public void mostrarTodosLosLibros(){
+    
+    }
+    public void mostrarTodosLosUsuarios(){
+    
+    }
+    public void mostrarPrestamosActivos(){
+    
+    }
+    public void buscarLibroPorIsbn(){
+    
+    }
+    public void buscarUsuarioPorId(){
+    
+    }
+    
+    public void listaPrestamosVencidos(){
+    
+    }
 }
