19cb613c20c3ada11d3649a243775d374bbf59ea
xnocc <alonsomikoulits@gmail.com>
2024-09-28 10:59:44 -0500

    estudiantes 2 y lista

---
+++ src/tallerlinked/Estudiantes2.java
@@ -0,0 +1,47 @@
+
+package tallerlinked;
+
+
+public class Estudiantes2 {
+    String nombre;
+    int ID;
+    Double promedio;
+
+    public Estudiantes2(String nombre, int ID, Double promedio) {
+        this.nombre = nombre;
+        this.ID = ID;
+        this.promedio = promedio;
+    }
+    public Estudiantes2() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public Double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(Double promedio) {
+        this.promedio = promedio;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiantes2{" + "nombre=" + nombre + ", ID=" + ID + ", promedio=" + promedio + '}';
+    }
+    
+}
--- src/tallerlinked/ListaEstudiantes.java
+++ src/tallerlinked/ListaEstudiantes.java
@@ -1,6 +1,7 @@
 
 package tallerlinked;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 
 /**
@@ -9,24 +10,41 @@ import java.util.LinkedList;
  */
 public class ListaEstudiantes {
     LinkedList<Estudiantes> lista = new LinkedList<>();
+    LinkedList<Estudiantes2> otraLista =new LinkedList<>();
     
     public void agregarEstudiante(Estudiantes estudiante){
        lista.addLast(estudiante);
        
     }
     
-    
-    public void insertarEstudiante(int index, Estudiantes estudiante){
-        lista.addFirst (estudiante);
+    public void agregarEstudiante2(Estudiantes2 estudiante){
+       otraLista.addLast(estudiante);
+       
     }
     
     
+    public void insertarEstudiante(int index, Estudiantes estudiante){
+        lista.add (index, estudiante);
+    }
     
+    public void insertarEstudiante2(int index, Estudiantes2 estudiante){
+        otraLista.add (index, estudiante);
+    }    
     
     public void eliminarEstudiante(int id){
         for (Estudiantes estudiantes : lista) {
             if(estudiantes.getID()==id){
                 lista.remove(id);
+
+            }
+
+        }   
+    }
+    
+    public void eliminarEstudiante2(int id){
+        for (Estudiantes2 estudiantes : otraLista) {
+            if(estudiantes.getID()==id){
+                otraLista.remove(id);
 
             }
 
@@ -43,15 +61,37 @@ public class ListaEstudiantes {
         return 0;
         
     }
+    public int buscarEstudiante2(int id){
+        for (Estudiantes2 estudiantes : otraLista) {
+            if(estudiantes.getID()==id){
+                return id;
+            }
+            
+        }
+        return 0;
+        
+    }
     
-    public LinkedList obtenerTopEstudiantes(int n){
-        
-        return null;
-        
+    public LinkedList<Estudiantes> obtenerTopEstudiantes(int n) {
+    lista.sort(Comparator.comparingDouble(Estudiantes::getPromedio).reversed());
+    
+    return new LinkedList<>(lista.subList(0, Math.min(n, lista.size())));
+}
+    
+    public void fusionarListas(LinkedList<Estudiantes> otraLista) {
+        lista.addAll(otraLista);
     }
     
     public void imprimirLista(){
         for (Estudiantes estudiantes : lista) {
+            System.out.println(estudiantes.toString());
+            
+        }
+        
+        
+    }
+    public void imprimirLista2(){
+        for (Estudiantes2 estudiantes : otraLista) {
             System.out.println(estudiantes.toString());
             
         }
@@ -70,6 +110,15 @@ public class ListaEstudiantes {
         }
     }
     
+    public void eliminarReprobados2(){
+        for (Estudiantes2 estudiantes : otraLista) {
+            if(estudiantes.getID()<30){
+                otraLista.remove(estudiantes);
+                
+            }
+            
+        }
+    }
     
     
     
